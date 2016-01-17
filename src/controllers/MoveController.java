7e4312054b1bdc0ac0757a58577f54ce8d5623c8
Alex <Alexp2019@gmail.com>
2016-01-17 21:52:16 +0300

    Is just the beginning

---
+++ src/controllers/MoveController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import io.xo.Field;
+import io.xo.Figure;
+import io.xo.exception.AlreadyOccupiedException;
+import io.xo.exception.InvalidPointException;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure(final Field field, final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+
+        if (field.getFigure(point) != null){
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+    }
+}
