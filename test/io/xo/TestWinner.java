8f8c01298711e6b84eb70589b6c40f4e69d3d7ea
Alex <Alexp2019@gmail.com>
2016-01-18 21:39:39 +0300

    Last update

--- test/io/xo/TestWinner.java
+++ test/io/xo/TestWinner.java
@@ -11,11 +11,11 @@ public class TestWinner {
     public void Test1(){
         Field field = new Field(3);
         try {
-            field.setFigure(new Point(0,0), Figure.O);
-            field.setFigure(new Point(0,1), Figure.X);
-            field.setFigure(new Point(0,2), Figure.X);
-            field.setFigure(new Point(1,0), Figure.O);
-            field.setFigure(new Point(2,0), Figure.O);
+            field.setFigure(new Point(2, 0), Figure.X);
+            field.setFigure(new Point(1,1), Figure.X);
+            field.setFigure(new Point(0, 2), Figure.X);
+            field.setFigure(new Point(0,1), Figure.O);
+
 
         } catch (InvalidPointException e) {
             e.printStackTrace();
